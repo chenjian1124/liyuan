1d07223e5d3335d49f4b27711d69eb0dbd397fe4
陈建 <331006361@qq.com>
2026-01-11 15:44:21 +0800

    关于我们页面开发

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,239 @@
 <template>
   <section class="page">
-    <h1>关于</h1>
-    <p>简单的关于页面。</p>
+    <div class="banner">
+      <img src="@/assets/about/banner-bg.png" alt="banner" class="banner_img" />
+      <div class="banner_content">
+        <div class="banner_content_title">关于我们</div>
+        <div class="banner_content_desc">
+          立远恒通力争成为全球领先的综合物流解决方案提供商,<br />
+          通过我们的专业知识和完善的服务网络,为客户提供无缝的、高效的和可靠的物流服务。
+        </div>
+      </div>
+    </div>
+    <!-- 介绍 -->
+    <div class="about_intro">
+      <div class="layout_container">
+        <div class="about_intro_content—_top">
+          <div class="about_intro_content—_top_left">
+            <div class="about_intro_content—_top_left_title">立远恒通简介</div>
+            <div class="about_intro_content—_top_left_desc">
+              <p>
+                立远恒通集团是国家商务部批准的一级国际货运代理公司集团,同时也是交通运输部批准的具有无船承运业务经营资格的无船承运人。
+              </p>
+              <p>
+                公司先后在青岛、宁波、天津、广州等地区成立了公司独资管理的分支机构,目前公司员工达500+余人,为国内外客户提供专业的物流服务。
+              </p>
+              <p>
+                经过多年的发展,依托WCA的完善的服务网络,我们的服务覆盖拉丁美洲、北美洲、亚洲、欧洲和非洲市场,尤其在拉丁美洲航线上积累了非常丰富的经验并建立了强大的海外代理服务网络,公司目前是CMA/MSK/MSC/ONE/HPL/PIL/COSCO/OOCL/EMC/WHL/ZIM/HMM等多家船公司的战略合作伙伴,为客户提供稳定、高效、可靠、低碳的物流解决方案。
+              </p>
+            </div>
+          </div>
+          <div class="about_intro_content—_top_right">
+            <img
+              src="@/assets/about/intro.png"
+              alt="intro_top_right"
+              class="about_intro_content—_top_right_img"
+            />
+          </div>
+        </div>
+        <div class="about_intro_content—_bottom">
+          <div class="about_intro_content—_bottom_item" v-for="i in 5" :key="i">
+            <div class="about_intro_content—_bottom_item_title">公司愿景</div>
+            <div class="about_intro_content—_bottom_item_desc">
+              我们力争成为全球领先的综合物流解决方案提供商，通过我们的专业知识和完善的服务网络，为客户提供无缝的、高效的和可靠的物流服务。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 发展 -->
+    <div class="about_development">
+      <div class="layout_container">
+        <div class="title_content"><Title title="发展历程" /></div>
+      </div>
+      <div class="about_development_content">
+        <img
+          src="@/assets/about/development.png"
+          alt="development"
+          class="about_development_content_img"
+        />
+      </div>
+    </div>
+    <!-- 代理地图 -->
+    <div class="about_agent_map">
+      <div class="layout_container">
+        <div class="title_content"><Title title="全球代理地图" /></div>
+        <img
+          src="@/assets/about/agent_map.png"
+          alt="agent_map"
+          class="agent_map_img"
+        />
+      </div>
+    </div>
+    <!-- 客户 -->
+    <CustomerSwiper />
+    <AboutUs />
   </section>
 </template>
 
+<script setup>
+import Title from "@/components/Title.vue";
+import AboutUs from "@/components/AboutUs.vue";
+import CustomerSwiper from "@/components/CustomerSwiperAbout.vue";
+</script>
+
 <style lang="less" scoped>
 .page {
-  padding: 24px;
+  .banner {
+    position: relative;
+    width: 100%;
+    height: 446px;
+    .banner_img {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      z-index: 1;
+    }
+    .banner_content {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+      z-index: 2;
+    }
+    .banner_content_title {
+      font-size: 86px;
+      line-height: 100px;
+      font-weight: 700;
+      color: #fff;
+      margin-bottom: 20px;
+    }
+    .banner_content_desc {
+      font-size: 24px;
+      line-height: 30px;
+      font-weight: 400;
+      color: #fff;
+    }
+  }
+}
+
+.about_intro {
+  // 一半白色，一半灰色
+  background: linear-gradient(to bottom, #f5f5f5 80%, #424f65 20%);
+  padding: 140px 0;
+  .about_intro_content—_top {
+    display: flex;
+    justify-content: space-between;
+    gap: 60px;
+    .about_intro_content—_top_left {
+      width: 660px;
+      .about_intro_content—_top_left_title {
+        margin: 40px 0;
+        font-size: 40px;
+        line-height: 50px;
+        font-weight: 700;
+        color: #000;
+      }
+      .about_intro_content—_top_left_desc {
+        font-size: 24px;
+        line-height: 32px;
+        font-weight: 400;
+        color: #333;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        white-space: pre-line;
+        word-break: break-all;
+      }
+    }
+    .about_intro_content—_top_right {
+      flex: 1;
+      .about_intro_content—_top_right_img {
+        display: block;
+        width: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+  .about_intro_content—_bottom {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 140px;
+    gap: 30px;
+    text-align: center;
+    .about_intro_content—_bottom_item {
+      flex: 1;
+      padding: 32px;
+      background-color: #fff;
+      border-bottom: 8px solid #41d62b;
+      position: relative;
+      &::before {
+        content: "";
+        position: absolute;
+        height: 8px;
+        background-color: #ff9124;
+        bottom: -8px;
+        left: 0;
+        width: 50%;
+      }
+      &::after {
+        content: "";
+        position: absolute;
+        height: 8px;
+        background-color: #2e9adb;
+        bottom: -8px;
+        left: 20%;
+        right: 20%;
+        width: 60%;
+        transform: skew(20deg);
+      }
+      .about_intro_content—_bottom_item_title {
+        font-size: 32px;
+        margin-bottom: 20px;
+        font-weight: 700;
+        color: #000;
+      }
+      .about_intro_content—_bottom_item_desc {
+        font-size: 16px;
+        line-height: 24px;
+        font-weight: 400;
+        color: #333;
+        text-align: left;
+      }
+    }
+  }
+}
+
+.about_development {
+  .title_content {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 120px 0;
+  }
+  .about_development_content {
+    .about_development_content_img {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+
+.about_agent_map {
+  .title_content {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 120px 0;
+  }
+  .agent_map_img {
+    display: block;
+    width: 100%;
+  }
 }
 </style>
-
--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -1,44 +1,46 @@
 <template>
   <section class="page">
-    <div class="layout_container solution_content">
-      <Title
-        title="OOG项目物流"
-        desc="专业高效，确保超限货物安全、及时地到达目的地。"
-      />
-      <div class="content">
-        <div class="content_left">
-          <div class="content_left_title">
-            OOG特种箱凭借卓越性能和可靠品质，成为全球物流运输的关键助力。无论是在繁忙的港口，还是偏远的航线，OOG特种箱始终以高效、安全的服务，为全球贸易与经济增长注入新动力。
+    <div class="solution_bg">
+      <div class="layout_container solution_content">
+        <Title
+          title="OOG项目物流"
+          desc="专业高效，确保超限货物安全、及时地到达目的地。"
+        />
+        <div class="content">
+          <div class="content_left">
+            <div class="content_left_title">
+              OOG特种箱凭借卓越性能和可靠品质，成为全球物流运输的关键助力。无论是在繁忙的港口，还是偏远的航线，OOG特种箱始终以高效、安全的服务，为全球贸易与经济增长注入新动力。
+            </div>
+            <div class="content_left_title">
+              立远恒通依托先进设备、精准装载与加固技术，以及多年的行业深耕经验，全面保障OOG项目物流的高效执行与运输安全。
+            </div>
           </div>
-          <div class="content_left_title">
-            立远恒通依托先进设备、精准装载与加固技术，以及多年的行业深耕经验，全面保障OOG项目物流的高效执行与运输安全。
-          </div>
-        </div>
-        <div class="content_right">
-          <div class="content_right_list" v-for="i in 2" :key="i">
-            <div class="content_right_list_item">
-              <img
-                src="@/assets/solution/1.png"
-                alt="solution1"
-                class="content_right_list_item_img"
-              />
-              <div class="content_right_list_item_content">
-                <div class="content_right_list_item_title">丰富运输经验</div>
-                <div class="content_right_list_item_desc">
-                  拥有多年超限货物运输经验，深度理解各类OOG项目的复杂需求，助您轻松应对运输难题。
+          <div class="content_right">
+            <div class="content_right_list" v-for="i in 2" :key="i">
+              <div class="content_right_list_item">
+                <img
+                  src="@/assets/solution/1.png"
+                  alt="solution1"
+                  class="content_right_list_item_img"
+                />
+                <div class="content_right_list_item_content">
+                  <div class="content_right_list_item_title">丰富运输经验</div>
+                  <div class="content_right_list_item_desc">
+                    拥有多年超限货物运输经验，深度理解各类OOG项目的复杂需求，助您轻松应对运输难题。
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="content_right_list_item">
-              <img
-                src="@/assets/solution/1.png"
-                alt="solution1"
-                class="content_right_list_item_img"
-              />
-              <div class="content_right_list_item_content">
-                <div class="content_right_list_item_title">丰富运输经验</div>
-                <div class="content_right_list_item_desc">
-                  拥有多年超限货物运输经验，深度理解各类OOG项目的复杂需求，助您轻松应对运输难题。
+              <div class="content_right_list_item">
+                <img
+                  src="@/assets/solution/1.png"
+                  alt="solution1"
+                  class="content_right_list_item_img"
+                />
+                <div class="content_right_list_item_content">
+                  <div class="content_right_list_item_title">丰富运输经验</div>
+                  <div class="content_right_list_item_desc">
+                    拥有多年超限货物运输经验，深度理解各类OOG项目的复杂需求，助您轻松应对运输难题。
+                  </div>
                 </div>
               </div>
             </div>
@@ -47,17 +49,148 @@
       </div>
     </div>
     <!-- 业务 -->
-    <Business />
+    <Business style="padding-top: 100px" />
+    <!--化工品 -->
+    <div class="solution_chemical">
+      <div class="layout_container solution_chemical_content">
+        <Title
+          title="化工品物流"
+          desc="专业守护每一程，安全运送每一滴化工品。"
+        />
+        <div class="solution_chemical_title">
+          <span class="text-orange">专业、安全、全球覆盖</span>的运输解决方案
+        </div>
+        <div class="solution_chemical_desc">
+          立远恒通在化工品与危险品物流及仓储操作方面拥有深厚经验，尤其在锂电池运输的加固方案及医药化工品处理领域表现出专业优势。通过成熟的技术和严格的操作流程，我们为客户提供安全可靠的物流及仓储解决方案，确保货物在运输和存储过程中的安全无损。
+        </div>
+        <img
+          src="@/assets/solution/chemica.png"
+          alt="chemical"
+          class="solution_chemical_img"
+        />
+      </div>
+    </div>
+    <!-- 汽车 -->
+    <div class="solution_car">
+      <div class="layout_container solution_car_content">
+        <Title
+          title="汽车物流"
+          desc="提供端到端的一站式解决方案，包括起运港装箱、国际海运以及目的港拆箱。"
+        />
+        <div class="solution_car_title">
+          <span class="text-orange"
+            >全程高效、专业，确保运输每个环节的顺畅衔接。</span
+          >
+        </div>
+        <div class="solution_car_desc">
+          立远恒通专注于汽车运输服务，采用主流的滚装（RO-RO）方式以及框架运输模式，提供灵活高效的解决方案。
+          图中展示的是中远海特的专利产品。<br />
+          在汽车运输领域，我司拥有一支经验丰富、技术过硬的专业运输团队，确保每次运输任务安全可靠、高效完成。
+        </div>
+        <div class="solution_car_list">
+          <div class="solution_car_list_item">
+            <img
+              src="@/assets/solution/car1.png"
+              alt="car"
+              class="solution_car_list_item_img"
+            />
+            <div class="solution_car_list_item_title">
+              立远恒通为中远海特的专利产品，使用汽车运输框架进行运输
+            </div>
+          </div>
+          <div class="solution_car_list_item">
+            <img
+              src="@/assets/solution/car2.png"
+              alt="car"
+              class="solution_car_list_item_img"
+            />
+            <div class="solution_car_list_item_title">
+              立远恒通为车企运输电车到智利
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 物流 -->
+    <div class="solution_logistics">
+      <div class="layout_container solution_logistics_content">
+        <Title title="物流智道" desc="先进灵活，功能全面。" />
+        <div class="solution_logistics_info">
+          <div class="solution_logistics_info_left">
+            <img
+              src="@/assets/solution/logistics.png"
+              alt="logistics"
+              class="solution_logistics_info_left_img"
+            />
+            <div class="solution_logistics_info_left_text">
+              <div class="solution_logistics_info_left_text_title">
+                互联融合·科技创新
+              </div>
+              <div class="solution_logistics_info_left_text_desc">
+                沉淀行业数十年经验,将智能技术融入国际物流服务引领服务变革。
+              </div>
+            </div>
+          </div>
+          <div class="solution_logistics_info_right">
+            <div
+              class="solution_logistics_info_right_item"
+              v-for="i in 4"
+              :key="i"
+            >
+              <div class="solution_logistics_info_right_item_icon">
+                <img
+                  src="@/assets/solution/logistics_icon1.png"
+                  alt="logistics"
+                  class="solution_logistics_info_right_item_img"
+                />
+              </div>
+              <div class="solution_logistics_info_right_item_text">
+                <div class="solution_logistics_info_right_item_text_title">
+                  数据可视化
+                </div>
+                <div class="solution_logistics_info_right_item_text_desc">
+                  展示时间、地点、货物类型、运输方等纬度帮助用户进行综合分析。用户可以根据需要选择不同的数据维度进行组合和比较，使系统能够适应不同用户和不同业务场景的需求。
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 顾问 -->
+    <div class="solution_advisor">
+      <div class="layout_container solution_advisor_content">
+        <Title title="专业顾问" desc="我们专业的团队，为您提供最优质的服务" />
+        <div class="solution_advisor_list">
+          <div class="solution_advisor_list_item" v-for="i in 4" :key="i">
+            <img
+              src="@/assets/solution/advisor1.png"
+              alt="advisor"
+              class="solution_advisor_list_item_img"
+            />
+            <div class="solution_advisor_list_item_text">
+              <div class="solution_advisor_list_item_text_name">Jason李杰</div>
+              <div class="solution_advisor_list_item_text_site">
+                立远恒通 宁波
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 关于我们 -->
+    <AboutUs />
   </section>
 </template>
 
 <script setup>
 import Title from "@/components/Title.vue";
 import Business from "@/components/Business.vue";
+import AboutUs from "@/components/AboutUs.vue";
 </script>
 
 <style lang="less" scoped>
-.page {
+.solution_bg {
   background: url("@/assets/solution/bg.png") no-repeat center center;
   background-size: cover;
 }
@@ -114,6 +247,218 @@ import Business from "@/components/Business.vue";
           color: #fff;
         }
       }
+    }
+  }
+}
+
+//
+.solution_chemical {
+  // 一半白色，一半灰色
+  background: linear-gradient(to bottom, #f5f5f5 75%, #424f65 25%);
+  padding-top: 100px;
+  .solution_chemical_content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+  }
+  .solution_chemical_title {
+    font-size: 32px;
+    line-height: 40px;
+    font-weight: 700;
+    color: #000;
+  }
+  .solution_chemical_desc {
+    max-width: 1000px;
+    font-size: 20px;
+    line-height: 24px;
+    color: #333;
+    text-align: center;
+  }
+  .solution_chemical_img {
+    width: 100%;
+    object-fit: cover;
+  }
+}
+
+//
+.solution_car {
+  background: #fff;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  .solution_car_content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+  }
+  .solution_car_title {
+    font-size: 32px;
+    line-height: 40px;
+    font-weight: 700;
+    color: #000;
+  }
+  .solution_car_desc {
+    max-width: 1000px;
+    font-size: 20px;
+    line-height: 30px;
+    color: #333;
+    text-align: center;
+  }
+  .solution_car_list {
+    display: flex;
+    gap: 40px;
+    .solution_car_list_item {
+      flex: 1;
+    }
+    .solution_car_list_item_img {
+      display: block;
+      width: 100%;
+      object-fit: cover;
+    }
+    .solution_car_list_item_title {
+      background-color: #424f65;
+      padding: 20px;
+      font-size: 20px;
+      font-weight: 700;
+      color: #fff;
+    }
+  }
+}
+
+//
+.solution_logistics {
+  background: #f5f5f5;
+  padding: 100px 0;
+  .solution_logistics_content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+  }
+  .solution_logistics_info {
+    display: flex;
+    gap: 100px;
+  }
+  .solution_logistics_info_left {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+    .solution_logistics_info_left_img {
+      display: block;
+      width: 100%;
+      object-fit: cover;
+    }
+    .solution_logistics_info_left_text {
+      display: flex;
+      flex-direction: column;
+      padding: 32px 0;
+      background-color: @orange;
+      color: #fff;
+      text-align: center;
+    }
+    .solution_logistics_info_left_text_title {
+      font-size: 32px;
+      line-height: 40px;
+      font-weight: 700;
+      color: #fff;
+    }
+    .solution_logistics_info_left_text_desc {
+      font-size: 20px;
+      line-height: 24px;
+      color: #fff;
+    }
+  }
+  .solution_logistics_info_right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    .solution_logistics_info_right_item {
+      display: flex;
+      gap: 40px;
+    }
+    .solution_logistics_info_right_item_icon {
+      width: 100px;
+      height: 100px;
+      background-color: @orange;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 50%;
+      .solution_logistics_info_right_item_img {
+        display: block;
+      }
+    }
+    .solution_logistics_info_right_item_text {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+    }
+    .solution_logistics_info_right_item_text_title {
+      font-size: 32px;
+      line-height: 40px;
+      font-weight: 700;
+      color: #000;
+    }
+    .solution_logistics_info_right_item_text_desc {
+      font-size: 20px;
+      line-height: 24px;
+      font-weight: 400;
+      color: #333;
+    }
+  }
+}
+
+//
+.solution_advisor {
+  background: #fff;
+  padding: 100px 0;
+  .solution_advisor_content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 120px;
+  }
+  .solution_advisor_list {
+    display: flex;
+    gap: 40px;
+    .solution_advisor_list_item {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    .solution_advisor_list_item_img {
+      display: block;
+      width: 96%;
+      object-fit: cover;
+    }
+    .solution_advisor_list_item_text {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background: #2e9adb;
+      padding: 10px 20px;
+      color: #fff;
+    }
+    .solution_advisor_list_item_text_name {
+      font-size: 40px;
+      line-height: 50px;
+      font-weight: 700;
+    }
+    .solution_advisor_list_item_text_site {
+      font-size: 24px;
+      line-height: 32px;
+      font-weight: 400;
     }
   }
 }
